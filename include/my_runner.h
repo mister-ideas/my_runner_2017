ae86a1ab2e03919b88d7f346b2d5632d3b879952
Vincent Attali <vincent.attali@epitech.eu>
2017-12-28 13:34:51 +0100

    "added create and destory obj functions"

--- include/my_runner.h
+++ include/my_runner.h
@@ -8,4 +8,16 @@
 #ifndef MY_RUNNER_H_
 #define MY_RUNNER_H_
 
+typedef struct object {
+	sfSprite *sprite;
+	sfTexture *texture;
+	sfVector2f *pos;
+	sfVector2f *move;
+	sfIntRect *rect;
+} object_t;
+
+void destroy_obj(object_t *object);
+
+object_t *new_obj(char *path, sfVector2f pos, sfVector2f move, sfIntRect rect);
+
 #endif
--- src/my_runner.c
+++ src/my_runner.c
@@ -5,7 +5,38 @@
 ** my_runner.c
 */
 
-int main(void)
+#include <SFML/Graphics.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "my.h"
+#include "my_runner.h"
+
+object_t *new_obj(char *path, sfVector2f pos, sfVector2f move, sfIntRect rect)
 {
+	object_t *object = malloc(sizeof(object));
+
+	object->texture = sfTexture_createFromFile(path, NULL);
+	if (!object->texture)
+		return (NULL);
+	object->sprite = sfSprite_create();
+	sfSprite_setTexture(object->sprite, object->texture, sfTrue);
+	sfSprite_setTextureRect(object->sprite, rect);
+	sfSprite_setPosition(object->sprite, pos);
+	sfSprite_move(object->sprite, move);
+	return (object);
+}
+
+void destroy_obj(object_t *object)
+{
+	sfTexture_destroy(object->texture);
+	sfSprite_destroy(object->sprite);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (84);
+	if (av[1][0] == '-' && av[1][1] == 'h')
+		return (1);
 	return (0);
 }
