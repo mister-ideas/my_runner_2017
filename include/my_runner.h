6f80835cf5cf18beabb0c92afc44ed26c81824a6
Vincent Attali <vincent.attali@epitech.eu>
2017-12-30 10:48:19 +0100

    "restart"

--- include/my_runner.h
+++ include/my_runner.h
@@ -13,17 +13,8 @@ typedef struct runner {
 	sfEvent event;
 } runner_t;
 
-typedef struct object {
-	sfSprite *sprite;
-	sfTexture *texture;
-	sfVector2f *pos;
-	sfIntRect *rect;
-} object_t;
-
 void check_events(runner_t *runner);
-void destroy_obj(object_t *object);
 
 sfRenderWindow *window_create(unsigned int width, unsigned int height);
-object_t *new_obj(const char *path, sfVector2f pos, sfIntRect rect);
 
 #endif
--- src/objects.c
+++ src/objects.c
@@ -11,23 +11,3 @@
 #include "my.h"
 #include "my_runner.h"
 
-object_t *new_obj(const char *path, sfVector2f pos, sfIntRect rect)
-{
-	object_t *object = malloc(sizeof(object));
-
-	object->texture = sfTexture_createFromFile(path, NULL);
-	if (!object->texture)
-		return (NULL);
-	object->sprite = sfSprite_create();
-	sfSprite_setTexture(object->sprite, object->texture, sfTrue);
-	sfSprite_setTextureRect(object->sprite, rect);
-	sfSprite_setPosition(object->sprite, pos);
-	return (object);
-}
-
-void destroy_obj(object_t *object)
-{
-	sfTexture_destroy(object->texture);
-	sfSprite_destroy(object->sprite);
-	free(object);
-}
