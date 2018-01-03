792f1cd433553fca9fce03e8dadfaad7675b64b5
Vincent Attali <vincent.attali@epitech.eu>
2018-01-03 14:16:11 +0100

    "modified road texture"

--- src/events.c
+++ src/events.c
@@ -23,9 +23,14 @@ void check_window(runner_t *runner)
 
 void check_layers(parallax_t *parallax)
 {
+	parallax->back_p = sfSprite_getPosition(parallax->back_s);
 	parallax->m1_p = sfSprite_getPosition(parallax->m1_s);
 	parallax->m2_p = sfSprite_getPosition(parallax->m2_s);
 	parallax->m3_p = sfSprite_getPosition(parallax->m3_s);
+	parallax->road_p = sfSprite_getPosition(parallax->road_s);
+	if (parallax->back_p.x < -1920)
+		parallax->back_p.x = 0;
+	sfSprite_setPosition(parallax->back_s, parallax->back_p);
 	if (parallax->m1_p.x < -1920)
 		parallax->m1_p.x = 0;
 	sfSprite_setPosition(parallax->m1_s, parallax->m1_p);
@@ -35,6 +40,9 @@ void check_layers(parallax_t *parallax)
 	if (parallax->m3_p.x < -1920)
 		parallax->m3_p.x = 0;
 	sfSprite_setPosition(parallax->m3_s, parallax->m3_p);
+	if (parallax->road_p.x < -1920)
+		parallax->road_p.x = 0;
+	sfSprite_setPosition(parallax->road_s, parallax->road_p);
 }
 
 void check_events(runner_t *runner, parallax_t *parallax)
